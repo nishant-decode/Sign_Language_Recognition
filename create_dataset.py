35badc7236d4f84e7527d05a2831a3b7c8c04b6c
Nishant <alpha.5188457@gmail.com>
2023-09-08 21:03:16 +0530

    Sign Language Recognition

--- create_dataset.py
+++ create_dataset.py
@@ -17,33 +17,34 @@ DATA_DIR = './data'
 data = []
 labels = []
 for dir_ in os.listdir(DATA_DIR):
-    for img_path in os.listdir(os.path.join(DATA_DIR, dir_)):
-        data_aux = []
+    if dir_ != '.DS_Store' and dir_ != '.gitignore':
+        for img_path in os.listdir(os.path.join(DATA_DIR, dir_)):
+            data_aux = []
 
-        x_ = []
-        y_ = []
+            x_ = []
+            y_ = []
 
-        img = cv2.imread(os.path.join(DATA_DIR, dir_, img_path))
-        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            img = cv2.imread(os.path.join(DATA_DIR, dir_, img_path))
+            img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-        results = hands.process(img_rgb)
-        if results.multi_hand_landmarks:
-            for hand_landmarks in results.multi_hand_landmarks:
-                for i in range(len(hand_landmarks.landmark)):
-                    x = hand_landmarks.landmark[i].x
-                    y = hand_landmarks.landmark[i].y
+            results = hands.process(img_rgb)
+            if results.multi_hand_landmarks:
+                for hand_landmarks in results.multi_hand_landmarks:
+                    for i in range(len(hand_landmarks.landmark)):
+                        x = hand_landmarks.landmark[i].x
+                        y = hand_landmarks.landmark[i].y
 
-                    x_.append(x)
-                    y_.append(y)
+                        x_.append(x)
+                        y_.append(y)
 
-                for i in range(len(hand_landmarks.landmark)):
-                    x = hand_landmarks.landmark[i].x
-                    y = hand_landmarks.landmark[i].y
-                    data_aux.append(x - min(x_))
-                    data_aux.append(y - min(y_))
+                    for i in range(len(hand_landmarks.landmark)):
+                        x = hand_landmarks.landmark[i].x
+                        y = hand_landmarks.landmark[i].y
+                        data_aux.append(x - min(x_))
+                        data_aux.append(y - min(y_))
 
-            data.append(data_aux)
-            labels.append(dir_)
+                data.append(data_aux)
+                labels.append(dir_)
 
 f = open('data.pickle', 'wb')
 pickle.dump({'data': data, 'labels': labels}, f)
